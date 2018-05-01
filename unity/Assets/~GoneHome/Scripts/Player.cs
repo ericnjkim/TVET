0201c1b8fb271ff62334815be9f1167d933d8ebd
eric.kim39 <eric.kim39@600900J211DE405>
2018-05-01 17:48:12 +1000

    Created visual studio project

---
+++ unity/Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+[] - brackets
+{} - braces
+() - parenthesis
+
+   Hot Keys:
+   Clean code ctrl + K + D
+   Fold code ctrl + M + O
+   Unfold code ctrl + M + P
+
+*/
+public class Player : MonoBehaviour
+{
+    public string message = "Hello World!";
+    public Rigidbody rigid;
+
+    float speed = 5;
+	// Use this for initialization
+	void Start ()
+    {
+        rigid.AddForce(Vector3.forward * speed);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Hello World!");
+        }
+    }
+}
---
+++ visualstudio/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message);
+            Console.WriteLine(message);
+            Console.WriteLine(message);
+
+            message = "10";
+
+            Console.WriteLine(message);
+            Console.WriteLine(message);
+            Console.WriteLine(message);
+
+            Console.ReadLine(); // Polls for input
+
+        }
+    }
+}
