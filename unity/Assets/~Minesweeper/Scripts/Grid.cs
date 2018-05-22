915217484e6bf7962f26cce30e536503f2279b32
eric.kim39 <eric.kim39@600900J211DE405>
2018-05-22 17:54:16 +1000

    Started working on Minesweeper

---
+++ unity/Assets/~Minesweeper/Scripts/Grid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10, height = 10;
+        public float spacing = .155f;
+
+        private Tile[,] tiles;
+    }
+}
---
+++ unity/Assets/~Minesweeper/Scripts/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Minesweeper
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        public int x, y;
+        public bool isMine = false;
+        public bool isRevealed = false;
+        [Header("References")]
+        public Sprite[] emptySprites;
+        public Sprite[] mineSprites;
+
+        private SpriteRenderer rend;
+        
+        void Start()
+        {
+            isMine = Random.value < .05f;
+        }
+        void Awake()
+        {
+            rend = GetComponent<SpriteRenderer>();
+        }
+
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            isRevealed = true;
+
+            if (isMine)
+            {
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+    }
+}
